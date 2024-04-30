3192643a4a487859d1637c90666e1d15019bb225
k3vinszn <kevinriscado98@gmail.com>
2024-04-30 18:27:17 +0000

    Commit inicial

---
+++ Source/MovementProj/MovementProj.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MovementProj.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MovementProj, "MovementProj" );
